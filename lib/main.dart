391ef207fa601c30df385289427258296bfc767c
h_shakeri <hamidrezash1384@gmail.com>
2024-10-16 17:32:02 +0330

    Added custom image selection and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:math';
 
+import 'package:desktop_drop/desktop_drop.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
 import 'package:flutter/services.dart';
@@ -14,9 +15,10 @@ class App extends StatelessWidget {
   const App({super.key});
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
+      theme: ThemeData.dark(),
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
@@ -31,21 +33,30 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
   late Ticker _ticker;
   final particleArray = <Particle>[];
-  late final adjustX = width / 2 - 312;
-  late final adjustY = -height / 2 + 150;
+  late final adjustX = 0;
+  // late final adjustX = width / 2 - 312;
+  // late final adjustY = -height / 2 + 150;
+  late final adjustY = 0;
   bool showImage = true;
   final mouse = Mouse();
   late final width = MediaQuery.sizeOf(context).width;
   late final height = MediaQuery.sizeOf(context).height;
+  int offset = 4;
+  int pixelSize = 4;
+  img.Image? lastImage;
 
-  Future<void> getImagePixels() async {
-    final ByteData data = await rootBundle.load('assets/images/Pagani.png');
-    final List<int> bytes = data.buffer.asUint8List();
-
-    final img.Image? image = img.decodeImage(Uint8List.fromList(bytes));
+  Future<void> getImagePixels({img.Image? newImage, bool animation = true}) async {
+    final ByteData data;
+    final Uint8List bytes;
+    img.Image? image = newImage;
+    if (image == null) {
+      data = await rootBundle.load('assets/images/Pagani.png');
+      bytes = data.buffer.asUint8List();
+      image = img.decodeImage(bytes);
+    }
     if (image != null) {
-      for (int y = 0; y < image.height; y += 3) {
-        for (int x = 0; x < image.width; x += 3) {
+      for (int y = 0; y < image.height; y += offset) {
+        for (int x = 0; x < image.width; x += offset) {
           img.Pixel pixel = image.getPixel(x, y);
           int r = pixel.r.toInt();
           int g = pixel.g.toInt();
@@ -56,8 +67,8 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
             final positionX = (x + adjustX).toDouble();
             final positionY = (y - adjustY).toDouble();
             particleArray.add(Particle(positionX, positionY, r, g, b));
-            particleArray.last.x = (Random().nextDouble() - 0.5) * 5 * width;
-            particleArray.last.y = (Random().nextDouble() - 0.5) * 5 * height;
+            particleArray.last.x = animation ? (Random().nextDouble() - 0.5) * 5 * width : positionX;
+            particleArray.last.y = animation ? (Random().nextDouble() - 0.5) * 5 * height : positionY;
           }
         }
       }
@@ -84,27 +95,189 @@ class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin
 
   @override
   Widget build(BuildContext context) {
-    return MouseRegion(
-      onHover: (event) {
-        mouse.x = event.position.dx;
-        mouse.y = event.position.dy;
-      },
-      child: GestureDetector(
-        onTap: () => showImage = !showImage,
-        child: Scaffold(
-          backgroundColor: Colors.black,
-          body: CustomPaint(
-            painter: ImagePainter(particleArray),
+    return Stack(
+      children: [
+        DropTarget(
+          onDragDone: (details) async {
+            if (details.files.first.name.contains('png') ||
+                details.files.first.name.contains('jpg') ||
+                details.files.first.name.contains('webp') ||
+                details.files.first.name.contains('jpeg')) {
+              particleArray.clear();
+              final bytes = await details.files.first.readAsBytes();
+              final image = img.decodeImage(bytes);
+              lastImage = image;
+              await getImagePixels(newImage: lastImage);
+            }
+          },
+          child: MouseRegion(
+            onHover: (event) {
+              // mouse.x = event.position.dx;
+              // mouse.y = event.position.dy;
+            },
+            child: GestureDetector(
+              // onTap: () => showImage = !showImage,
+              onTap: () async {
+                // for (int i = 0; i < particleArray.length; i++) {
+                //   particleArray[i].x = particleArray[i].baseX;
+                //   particleArray[i].y = particleArray[i].baseY;
+                // }
+                // pixelSize++;
+              },
+              child: Scaffold(
+                backgroundColor: Colors.black,
+                body: CustomPaint(
+                  painter: ImagePainter(
+                    particleArray,
+                    pixelSize: pixelSize.toInt(),
+                  ),
+                ),
+              ),
+            ),
           ),
         ),
-      ),
+        Align(
+          alignment: Alignment.topRight,
+          child: Padding(
+            padding: const EdgeInsets.only(right: 16, top: 16),
+            child: Material(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, top: 8),
+                    child: Text('pixel size : ${pixelSize.floor()}'),
+                  ),
+                  SizedBox(
+                    width: 240,
+                    child: Row(children: [
+                      SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: IconButton(
+                          onPressed: () {
+                            if (pixelSize <= 2) return;
+                            pixelSize--;
+                          },
+                          iconSize: 14,
+                          padding: EdgeInsets.zero,
+                          icon: const Icon(Icons.arrow_back_ios_rounded),
+                        ),
+                      ),
+                      Expanded(
+                        child: SliderTheme(
+                          data: SliderThemeData(
+                            trackHeight: 20,
+                            trackShape: const RectangularSliderTrackShape(),
+                            overlayShape: SliderComponentShape.noThumb,
+                            thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 0,
+                              disabledThumbRadius: 0,
+                            ),
+                          ),
+                          child: Slider(
+                            value: pixelSize.toDouble(),
+                            onChanged: (value) async {
+                              pixelSize = value.toInt();
+                            },
+                            max: 30,
+                            min: 2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: IconButton(
+                          onPressed: () {
+                            if (pixelSize.toInt() >= 30) return;
+                            pixelSize++;
+                          },
+                          iconSize: 14,
+                          padding: EdgeInsets.zero,
+                          icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                      ),
+                    ]),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 40, top: 8),
+                    child: Text('pixel offset : $offset'),
+                  ),
+                  SizedBox(
+                    width: 240,
+                    child: Row(children: [
+                      SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: IconButton(
+                          onPressed: () async {
+                            if (offset <= 2) return;
+                            offset--;
+                            particleArray.clear();
+                            await getImagePixels(newImage: lastImage, animation: false);
+                          },
+                          iconSize: 14,
+                          padding: EdgeInsets.zero,
+                          icon: const Icon(Icons.arrow_back_ios_rounded),
+                        ),
+                      ),
+                      Expanded(
+                        child: SliderTheme(
+                          data: SliderThemeData(
+                            trackHeight: 20,
+                            trackShape: const RectangularSliderTrackShape(),
+                            overlayShape: SliderComponentShape.noThumb,
+                            thumbShape: const RoundSliderThumbShape(
+                              enabledThumbRadius: 0,
+                              disabledThumbRadius: 0,
+                            ),
+                          ),
+                          child: Slider(
+                            value: offset.toDouble(),
+                            allowedInteraction: SliderInteraction.tapOnly,
+                            onChanged: (value) async {
+                              offset = value.toInt();
+                              particleArray.clear();
+                              await getImagePixels(newImage: lastImage, animation: false);
+                            },
+                            max: 30,
+                            min: 2,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: IconButton(
+                          onPressed: () async {
+                            if (offset >= 30) return;
+                            offset++;
+                            particleArray.clear();
+                            await getImagePixels(newImage: lastImage, animation: false);
+                          },
+                          iconSize: 14,
+                          padding: EdgeInsets.zero,
+                          icon: const Icon(Icons.arrow_forward_ios_rounded),
+                        ),
+                      ),
+                    ]),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
 
 class ImagePainter extends CustomPainter {
   final List<Particle> particleArray;
-  ImagePainter(this.particleArray);
+  final int pixelSize;
+  ImagePainter(this.particleArray, {this.pixelSize = 2});
 
   @override
   void paint(Canvas canvas, Size size) {
@@ -116,7 +289,7 @@ class ImagePainter extends CustomPainter {
       final dx = particle.x - particle.baseX;
       final dy = particle.y - particle.baseY;
       final distance = sqrt(dx * dx + dy * dy);
-      final fixedSize = particle.size + (2 - particle.size) * (1 - min((distance / 1000).abs(), 1));
+      final fixedSize = particle.size + (pixelSize - particle.size) * (1 - min((distance / 1000).abs(), 1));
 
       paint.color = Color.fromARGB(
           ((particle.density + (1 - particle.density) * (1 - min((distance / 1000), 1))) * 255).toInt(),
